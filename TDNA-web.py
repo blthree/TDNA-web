98075dd85b269c0206014452878ace254e8e7354
blthree <blthree89@gmail.com>
2017-08-15 12:42:43 -0400

    Initial commit

---
+++ TDNA-web.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, render_template
+from pick_tdna_primers import run_tdna_primers
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+    stock_num = ''
+    if request.method == 'POST':
+        stock_num = request.form['stock-num']
+
+        a = run_tdna_primers(stock_num)
+        return '\n'.join(a)
+    else:
+        return render_template('main.html')
+
+
+
+if __name__ == '__main__':
+    app.run()
