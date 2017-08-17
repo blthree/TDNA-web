9f7e847c2b56c3a4a1111b3ec2996c8ba2bbce4e
blthree <blthree89@gmail.com>
2017-08-17 14:48:48 -0400

    change sequence output formatting

--- TDNA-web.py
+++ TDNA-web.py
@@ -11,9 +11,8 @@ def hello_world():
         stock_num = request.form['stock-num']
 
         a = run_tdna_primers(stock_num)
-        b = primer_results(a[0]['name'],a[0]['Primers']['LP']['Sequence'], a[0]['Primers']['RP']['Sequence'], a[0]['Sequence'])
-        print(b.pretty_print())
-        return render_template('results.html', stock_num=stock_num, results=b.pretty_print())
+        b = primer_results(a[0]['name'],a[0]['Primers']['LP']['Sequence'], a[0]['Primers']['RP']['Sequence'], [a[0]['Primers']['LP']['Tm'], a[0]['Primers']['RP']['Tm']], a[0]['Sequence'])
+        return render_template('results.html', stock_num=stock_num, results=b.lines, p_name=b.name)
     else:
         return render_template('main.html')
 
--- classes.py
+++ classes.py
@@ -1,10 +1,13 @@
 class primer_results(object):
-    def __init__(self, poly_name, LP, RP, sequence):
+    def __init__(self, poly_name, LP, RP, primer_temps, sequence):
         self.name = poly_name
         self.LP = LP
         self.RP = RP
+        self.primer_temps = primer_temps
         self.sequence = sequence
+        self.lines = [poly_name, LP, RP, primer_temps, sequence]
     def pretty_print(self):
         s = self.name + '\n' + self.LP + '\n' + self.RP + '\n' + 'Sequence' + '\n'.join(self.sequence)
         return s
+
 
